3b3d271aed9114be63bc4ecc38b1011c6d47e939
louisnamb <63900979+louisnamb@users.noreply.github.com>
2024-07-22 12:54:56 +0930

    Add files via upload

---
+++ headers/pingpong.h
@@ -0,0 +1,39 @@
+#include <math.h>
+#include "../mlx/mlx.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+typedef struct pingpong
+{
+	int x;
+	int y;
+	int colour;
+	int	radius;
+	int	start;
+} t_pingpong;
+
+typedef struct window_info {
+	void				*mlx;
+	void				*img;
+	void				*win;
+	void				*addr;
+	int					bbp;
+	int					llen;
+	int					end;
+	int					width;
+	int					h;
+	int					min;
+	t_pingpong			*game;
+}	t_data;
+
+
+
+void	colorise(t_data *data, int x, int y, int colour);
+void	draw_circle(t_data *data, t_pingpong *ball);
+int		draw_bounce(t_data *data, t_pingpong *ball);
+void    solid_fill(t_data *data, int x, int start, int end, int colour);
+void    move_diagonally(t_data *data, t_pingpong *ball);
